16ab08ffe3d06d864ddf07a716f9df0dab301787
Manisha Awasthi <manisha@Manishas-MacBook-Pro.local>
2023-08-04 10:20:48 +0100

    always save artifact

--- features/pages/dice_base_page.rb
+++ features/pages/dice_base_page.rb
@@ -6,7 +6,6 @@ class DiceBasePage
 
   def select_menu_option(menu_options, value)
     menu_options.each { |option|
-      puts option.text
       if option.text.include? value
         option.click
         break
